59208cbdc7f05833cab5c17fdbda89b8d8a9ba20
haekalarif <haekal.rozikin@kitameraki.com>
2023-05-15 23:22:03 +0700

    update connection string database 15-05-2023 23.22

--- configuration/Database.ts
+++ configuration/Database.ts
@@ -3,7 +3,8 @@ import { MongoClientOptions } from "mongodb";
 import { Uri } from "../Uri";
 
 async function Connection() {
-  const stringConnection: string = `${Uri.dbUri}/${Uri.dbName}`;
+  // const stringConnection: string = `${Uri.dbUri}/${Uri.dbName}`;
+  const stringConnection: string = `mongodb+srv://arifrozikinh:WzRgtTTqpUEau8Qm@contact.k46fnau.mongodb.net/?retryWrites=true&w=majority`;
   try {
     mongoose.set('strictQuery', false)
     const connection = await mongoose.connect(stringConnection, { useNewUrlParser: true, useUnifiedTopology: true } as MongoClientOptions)
